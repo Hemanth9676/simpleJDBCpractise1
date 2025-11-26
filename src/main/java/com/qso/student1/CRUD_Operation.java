86d6d9b1c64ace8d2681b5b654ea7211822125cb
Yasarapu Hemanth <126438636+Hemanth9676@users.noreply.github.com>
2025-11-26 11:04:57 +0530

    Update closing connection message for clarity

--- src/main/java/com/qso/student1/CRUD_Operation.java
+++ src/main/java/com/qso/student1/CRUD_Operation.java
@@ -82,7 +82,7 @@ public class CRUD_Operation {
 
 					break;
 				case 5:
-					System.out.println("Closing connection...");
+					System.out.println("Closing connection Done...");
 					connection.close();
 					scn.close();
 					System.out.println("Exited Successfully Done!");
